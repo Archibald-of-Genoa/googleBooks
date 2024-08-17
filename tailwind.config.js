c138adb8ebee90478508039da89827a220bf3884
Archie <archibaldofgenoa@yandex.ru>
2024-08-18 01:08:18 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  content: [path.resolve(__dirname, "./index.html"), path.resolve(__dirname, "./src/**/*.{js,ts}")],
+  theme: {
+    colors: {
+      "body": "#ffffff",
+      "text-black": "#1C2A39",
+      "text-gray": "#5C6A79",
+      "counter-red": "#FF353A",
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
