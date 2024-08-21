643138ea71e9af93a8b926cda5a30ca6aa3f57ed
Archie <archibaldofgenoa@yandex.ru>
2024-08-21 17:53:33 +0300

    header init

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,27 @@ export default {
   content: [path.resolve(__dirname, "./index.html"), path.resolve(__dirname, "./src/**/*.{js,ts}")],
   theme: {
     colors: {
-      "body": "#ffffff",
+      body: "#ffffff",
       "text-black": "#1C2A39",
       "text-gray": "#5C6A79",
       "counter-red": "#FF353A",
     },
+
     fontFamily: {
-      sans: ['Open Sans', 'sans-serif'],
-      montserrat: ['Montserrat', 'sans-serif'],
+      sans: ["Open Sans", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          DEFAULT: "1440px",
+        },
+      },
+      width: {
+        '93': "23.25rem",
+      },
+    },
   },
   plugins: [],
 };
