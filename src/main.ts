ec6a8b3ad98cc4e19435c37a41a339286c6352ab
Archie <archibaldofgenoa@yandex.ru>
2024-10-17 17:59:17 +0300

    initial page rolled back

--- src/main.ts
+++ src/main.ts
@@ -14,146 +14,129 @@ document.addEventListener("DOMContentLoaded", async () => {
   new Slider();
 });
 
-document.addEventListener("DOMContentLoaded", async () => {
-  // Инициализация слайдера
-  new Slider();
+genresList.addEventListener("click", async (event) => {
+  const target = event.target as HTMLElement;
+  if (target.tagName === "LI") {
+    const category = target.getAttribute("data-category");
 
-  // Подгружаем книги для категории "Architecture" при загрузке страницы
-  await loadBooks("Architecture");
+    if (category) {
+      const cardsList = document.getElementById("cardsList");
+      const data = await searchBooks(category);
 
-  // Инициализируем обработчики событий для категорий жанров
-  initGenreListeners();
-});
+      if (data && cardsList) {
+        cardsList.innerHTML = "";
 
-async function loadBooks(category: string) {
-  const cardsList = document.getElementById("cardsList");
-  if (cardsList) {
-    const data = await searchBooks(category);
+        for (const { ...item } of data) {
+          const bookData = encodeURIComponent(JSON.stringify(item));
 
-    if (data) {
-      cardsList.innerHTML = ""; // Очищаем список книг
-
-      data.forEach((item) => {
-        const bookData = encodeURIComponent(JSON.stringify(item));
-        const html = String.raw;
-        const cardElement = document.createElement("div");
-
-        const ratingCount = item.ratingsCount
-          ? html`<div class="text-[10px]">${item.ratingsCount} review</div>`
-          : "";
-        const authors = item.authors
-          ? html`<h3 class="font-sans text-[10px] text-text-gray">
-              ${item.authors.join(", ")}
-            </h3>`
-          : "";
-        const description = item.description
-          ? html`<div
-              class="line-clamp-3 pt-4 font-sans text-[10px] text-text-gray"
-            >
-              ${item.description}
-            </div>`
-          : "";
-
-        let ratingTemplate = "";
-        if (item.averageRating) {
-          ratingTemplate = html`<div class="rating">
-            <div class="rating__body">
-              <div class="rating__active">
-                <div class="rating__items">
-                  <input class="rating__item" type="radio" />
-                  <input class="rating__item" type="radio" />
-                  <input class="rating__item" type="radio" />
-                  <input class="rating__item" type="radio" />
-                  <input class="rating__item" type="radio" />
-                </div>
-              </div>
-            </div>
-          </div>`;
-        }
-
-        const currencyTemplate =
-          item.retailPrice?.amount && item.retailPrice.currencyCode
-            ? html`<h2 class="mt-4 text-xs font-bold text-text-black">
-                ${item.retailPrice.amount} ${item.retailPrice.currencyCode}
-              </h2>`
+          const html = String.raw;
+          const cardElement = document.createElement("div");
+          const ratingCount = item.ratingsCount
+            ? html`<div class="text-[10px]">${item.ratingsCount} review</div>`
+            : "";
+          const authors = item.authors
+            ? html`<h3 class="font-sans text-[10px] text-text-gray">
+                ${item.authors.join(", ")}
+              </h3>`
+            : "";
+          const description = item.description
+            ? html`<div
+                class="line-clamp-3 pt-4 font-sans text-[10px] text-text-gray"
+              >
+                ${item.description}
+              </div>`
             : "";
 
-        const book = html`<div
-          class="flex min-h-[300px] justify-between gap-y-9"
-        >
-          <div class="flex w-1/2 items-center justify-center">
-            <img src="${item.image}" alt="${item.title}" />
-          </div>
+          let ratingTemplate = "";
+          if (item.averageRating) {
+            ratingTemplate = html` <div class="rating">
+              <div class="rating__body">
+                <div class="rating__active">
+                  <div class="rating__items">
+                    <input class="rating__item" type="radio" />
+                    <input class="rating__item" type="radio" />
+                    <input class="rating__item" type="radio" />
+                    <input class="rating__item" type="radio" />
+                    <input class="rating__item" type="radio" />
+                  </div>
+                </div>
+              </div>
+            </div>`;
+          }
 
-          <div class="flex w-1/2 flex-col items-start justify-center py-12">
-            ${authors}
-            <h2 class="text-base font-bold text-text-black">${item.title}</h2>
-            <div class="flex items-center justify-between gap-[6px]">
-              ${ratingTemplate} ${ratingCount}
+          const currencyTemplate =
+            item.retailPrice?.amount && item.retailPrice.currencyCode
+              ? html`<h2 class="mt-4 text-xs font-bold text-text-black">
+                  ${item.retailPrice.amount} ${item.retailPrice.currencyCode}
+                </h2>`
+              : "";
+
+          const book = html`
+            <div class="flex min-h-[300px] justify-between gap-y-9">
+              <div class="flex w-1/2 items-center justify-center">
+                <img src="${item.image}" alt="${item.title}" />
+              </div>
+
+              <div class="flex w-1/2 flex-col items-start justify-center py-12">
+                ${authors}
+                <h2 class="text-base font-bold text-text-black">
+                  ${item.title}
+                </h2>
+                <div class="flex items-center justify-between gap-[6px]">
+                  ${ratingTemplate} ${ratingCount}
+                </div>
+                ${description} ${currencyTemplate}
+                <button class="btn-primary mt-4" data-book="${bookData}">
+                  buy now
+                </button>
+              </div>
             </div>
-            ${description} ${currencyTemplate}
-            <button class="btn-primary mt-4" data-book="${bookData}">
-              buy now
-            </button>
-          </div>
-        </div>`;
+          `;
+          cardElement.innerHTML = book;
 
-        cardElement.innerHTML = book;
-        cardsList.append(cardElement);
+          if (cardsList) {
+            cardsList.append(cardElement);
+          }
 
-        // Обработка клика по кнопке "buy now"
-        const buyButton = cardElement.querySelector("button");
-        if (buyButton) {
-          buyButton.addEventListener("click", (event) => {
-            event.stopPropagation(); // Остановим всплытие события
+          const buyButton = cardElement.querySelector("button");
+          if (buyButton) {
+            buyButton.addEventListener("click", (event) => {
+              event.stopPropagation(); // Остановим всплытие события
 
-            const bookData = buyButton.getAttribute("data-book");
-            if (bookData) {
-              const bookObject = JSON.parse(decodeURIComponent(bookData));
-              console.log(bookObject);
+              const bookData = buyButton.getAttribute("data-book");
+              if (bookData) {
+                const bookObject = JSON.parse(decodeURIComponent(bookData));
+                console.log(bookObject);
 
-              const inCart = isBookInCart(bookObject); // Проверка, есть ли книга в корзине
-              updateButtonState(buyButton, inCart); // Обновляем состояние кнопки
+                const inCart = isBookInCart(bookObject); // Проверка, есть ли книга в корзине
+                updateButtonState(buyButton, inCart); // Обновляем состояние кнопки
 
-              if (inCart) {
-                removeFromCart(bookObject);
-                updateButtonState(buyButton, false);
-                console.log("Удалено из корзины:", getCart());
-              } else {
-                addToCart(bookObject);
-                updateButtonState(buyButton, true);
-                console.log("Добавлено в корзину:", getCart());
+                if (inCart) {
+                  removeFromCart(bookObject);
+                  updateButtonState(buyButton, false);
+                  console.log("Удалено из корзины:", getCart());
+                } else {
+                  addToCart(bookObject);
+                  updateButtonState(buyButton, true);
+                  console.log("Добавлено в корзину:", getCart());
+                }
               }
-            }
-          });
-        }
+            });
+          }
 
-        // Инициализация рейтинга
-        if (item.averageRating) {
-          const ratingActiveElement =
-            cardElement.querySelector<HTMLElement>(".rating__active");
-          if (ratingActiveElement) {
-            initRatings(item.averageRating);
-            const ratingActiveWidth = item.averageRating / 0.05;
-            ratingActiveElement.style.width = `${ratingActiveWidth}%`;
+          if (item.averageRating) {
+            const ratingActiveElement =
+              cardElement.querySelector<HTMLElement>(".rating__active");
+            if (ratingActiveElement) {
+              initRatings(item.averageRating);
+              const ratingActiveWidth = item.averageRating / 0.05;
+              ratingActiveElement.style.width = `${ratingActiveWidth}%`;
+            }
           }
         }
-      });
-    }
-  }
-}
-
-function initGenreListeners() {
-  genresList.addEventListener("click", async (event) => {
-    const target = event.target as HTMLElement;
-    if (target.tagName === "LI") {
-      const category = target.getAttribute("data-category");
-
-      if (category) {
-        await loadBooks(category); // Загрузка книг по выбранной категории
       }
-
-      handleActiveLi(target); // Обновляем активный класс для выбранного жанра
     }
-  });
-}
+
+    handleActiveLi(target);
+  }
+});
